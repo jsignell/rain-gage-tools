b548b93bfa4609372f53a4c4a324de83149bf7e3
jsignell <jsignell@gmail.com>
2015-10-30 12:54:54 +0000

    changed around class functions to allow better calling of specific times

--- raingage.py
+++ raingage.py
@@ -5,6 +5,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+def choose_group(df, interval, gage=None, m=None, h=None):
+    if gage is not None and type(gage) is not list and type(gage) is not tuple:
+        gage = [gage]
+    if m in range(1,13,1):
+        interval='seasonal'
+    elif h in range(0,24,1):
+        interval='diurnal'
+    if interval is 'seasonal':
+        if gage is None:
+            group = df.mean(axis=1).groupby(df.index.month)
+        elif set(gage) <= set(df.columns):
+            group = df[gage].groupby(df[gage].index.month)
+    elif interval is 'diurnal':
+        if gage is None:
+            group = df.mean(axis=1).groupby(df.index.hour)
+        elif set(gage) <= set(df.columns):
+            group = df[gage].groupby(df[gage].index.hour)
+
+    if m is None and h is None:
+        return group
+    elif m in range(1,13,1):
+        for name, df in group:
+            if name == m:
+                group1 = df.groupby(df.index.hour)
+    elif h in range(0,24,1):
+        for name, df in group:
+            if name == h:
+                group1 = df.groupby(df.index.month) 
+    return group1
+
+
 class RainGage:
     '''
     Base class for RainGage files
@@ -43,9 +74,6 @@ class RainGage:
         else:
             print "Error: incorrect init of RainGage"
 
-        self.df.months = self.df.groupby(self.df.index.month)
-        self.months = self.df.months.groups.keys()
-        self.get_gage_mean()
         self.reset_thresh()
 
     def get_files(self, year, name):
@@ -84,127 +112,120 @@ class RainGage:
                               parse_dates={'date_time': dates},
                               date_parser=dateparse, index_col=[0])
     
-    def get_gage_mean(self):
-        self.gage_mean = self.df.mean(axis=1)
-        self.gage_mean.months = self.gage_mean.groupby(self.gage_mean.index.month)
-        self.gage_mean.hours = self.gage_mean.groupby(self.gage_mean.index.hour)
-    
     def reset_thresh(self): 
         self.thresh = min([i for i in self.df[self.df.columns[0]] if i > 0])
         
     def get_wet(self):
-        self.wet = self.gage_mean >= self.thresh
-        self.wet.months = self.wet.groupby(self.wet.index.month)
-        self.wet.hours = self.wet.groupby(self.wet.index.hour)
+        self.wet = self.df >= self.thresh
+    
+    def create_title(self, title, interval='seasonal',
+                     gage=None, month=None, hour=None):
+        if gage is not None:
+            title = '{g}: '.format(g=gage)+title
+        if month is not None:
+            title = title + ' for Month {m} of'.format(m=month)
+        elif hour is not None:
+            title = title + ' for Hour {h} of'.format(h=hour) 
+        elif interval is 'seasonal':
+            title = title + ' for Months of'
+        elif interval is 'diurnal':
+            title = title + ' for Hours of'
+        full_title = (title +' {year}').format(ts='15min', year=self.year)
+        self.title = full_title
         
-    def get_prob_wet(self, interval='seasonal'):
+    def get_prob_wet(self, interval='seasonal', show_all=False,
+                     gage=None, month=None, hour=None, look_closer=None, lc=None):
         fig = plt.figure(figsize=(16,6))
         ax = fig.add_subplot(111)
-        
-        if interval is 'seasonal':
-            self.wet.months.mean().plot(kind='bar',legend=None, ax=ax)
-            ax.set_xlabel("Months of {year}".format(year=self.year))
 
-        elif interval is 'diurnal':
-            self.wet.hours.mean().plot(kind='bar',legend=None, ax=ax)
-            ax.set_xlabel("Hours of {year}".format(year=self.year))
-        
-        elif interval is 'all' or interval in self.months:
-            bar = []
-            monthly = [df[1] for df in self.wet.months]
-            for i, df in enumerate(monthly):
-                bar.append(df.groupby(df.index.hour).mean())
-            wet_hours_months = pd.DataFrame(bar).transpose()
-            wet_hours_months.columns = self.months
-            self.wet.hours.months = wet_hours_months
-            if interval in self.months:
-                self.wet.hours.months[interval].plot(kind='bar', ax=ax)
-                ax.set_xlabel("Hours of {interval}-{year}".format(interval=interval, year=self.year))
+        if show_all is False:
+            self.group = choose_group(self.wet, interval, gage, month, hour)
+            if look_closer is None:
+                self.group.mean().plot(kind='bar', ax=ax)
+            elif look_closer is not None:
+                self.group.mean()[look_closer].plot(kind='bar', ax=ax)
+            elif lc is not None:
+                self.group.mean()[look_closer].loc[lc].plot(kind='bar', ax=ax)
+
+        else:
+            if gage is None or len(gage) > 1:
+                gage = None
+                show_all = self.wet.mean(axis=1).groupby([self.wet.index.hour, self.wet.index.month]).mean().unstack()
+            elif len(gage) is 1:
+                show_all = self.wet[gage].groupby([self.wet.index.hour, self.wet.index.month]).mean().unstack() 
+            show_all.index.name='hours'
+            show_all.columns.name='months'
+            self.show_all = show_all
+            if month is None:
+                show_all.plot(kind='bar', ax=ax)
             else:
-                self.wet.hours.months.plot(kind='bar', ax=ax)
-                ax.set_xlabel("Hours of {year}".format(year=self.year))
+                if gage is None:
+                    if hour is None:
+                        show_all[month].plot(kind='bar', ax=ax)
+                    else:
+                        show_all[month].loc[hour].plot(kind='bar', ax=ax)
+                else:
+                    sliceit = zip(gage*len(month), month)
+                    if hour is None:
+                        show_all[sliceit].plot(kind='bar', ax=ax)
+                    else:
+                        show_all[sliceit].loc[hour].plot(kind='bar', ax=ax)
 
         ax.set_ylabel("Probability of wet 15min")
-        if not hasattr(self, 'prob_wet'):
-            self.prob_wet = {}
-        self.prob_wet.update({interval: fig})
+        self.create_title('Probability of wet 15min', interval, gage, month, hour)
+        plt.title(self.title)
 
-    def get_boxplots(self, df_list, fig, time_step='15min', interval='seasonal'):
-        foo = []
-        for i, df in enumerate(df_list):
-            df = df.resample(time_step)
+    def get_boxplots(self, time_step='15min', interval='seasonal', 
+                     gage=None, month=None, hour=None, look_closer=None):
+        self.group = choose_group(self.df, interval, gage, month, hour)
+        wet_rates = []
+        labels = []
+        for name, df in self.group:
+            if look_closer is not None:
+                if name not in look_closer:
+                    continue
+            elif interval is 'diurnal':
+                if name%2 == 1:  # if the hour is odd
+                    continue
+            df = df.resample('15min')
             df = df[df.T >= self.thresh]  # only keep wet days
             df = df * self.per_hour  # Make it a rain rate
-            if interval is 'seasonal':
-                df.name = df.index[0].month
-            if interval is 'diurnal' or interval in self.months:
-                df.name = df.index[0].hour
-            quan = df.quantile(np.arange(0,1.01,.01))
-            nrows = 1
-            ncols = len(df_list)
-            ax = fig.add_subplot(nrows, ncols, i+1)
-            df.plot(kind='box', sym='', whis=[10,90], meanline=True, logy=True, sharey=True, ax=ax)
-            if i == 0:
-                ax.set_ylabel('Rain Rate (mm/hr)')
-            foo.append(quan)
- 
-        if not hasattr(self, 'boxplots'):
-            self.boxplots = {}
-        if interval not in self.boxplots.keys():
-            self.boxplots.update({interval: {time_step: fig}})
-        self.boxplots[interval].update({time_step: fig})
+            wet_rates.append(df.values)
+            labels.append(name)
+        fig = plt.figure(figsize=(len(wet_rates)*4/3,4))
+        plt.boxplot(wet_rates, sym='', whis=[10,90], meanline=True, labels=labels)
+        plt.yscale('linear')
+        plt.ylabel('Rain Rate (mm/hr)')
+        title = '{ts} Rain Rate Distribution (excluding dry {ts})'
+        self.create_title(title, interval, gage, month, hour)
+        plt.title(self.title)
 
-        if not hasattr(self, 'quantiles'):
-            self.quantiles = {}
-        if interval not in self.quantiles.keys():
-            self.quantiles.update({interval: {time_step: pd.DataFrame(foo).transpose()}})
-        self.quantiles[interval].update({time_step: pd.DataFrame(foo).transpose()})
-
-    def get_distribution(self, time_step='15min', interval='seasonal'):
-        fig = plt.figure(figsize=(16,4))
-        ax = fig.add_subplot(111)
-        self.quantiles[interval][time_step].plot(logy=True,ax=ax, legend=None)
-        ax.set_ylabel('Rain Rate (mm/hr)')
-
-        if not hasattr(self, 'distribution'):
-            self.distribution = {}
-        if interval not in self.distribution.keys():
-            self.distribution.update({interval: {time_step:fig}})
-        self.distribution[interval].update({time_step: fig})
-    
-    def get_quantiles(self, time_step='15min', interval='seasonal'):
-        '''
-        interval={'seasonal', 'diurnal', int referring to specific month}
+    def get_distribution(self, time_step='15min', interval='seasonal', 
+                         gage=None, month=None, hour=None, look_closer=None):
+        self.group = choose_group(self.df, interval, gage, month, hour)
+        wet_rates = []
+        labels = []
+        foo = []
+        for name, df in self.group:
+            if look_closer is not None:
+                if name not in look_closer:
+                    continue
+            elif interval is 'diurnal':
+                if name%2 == 1:  # if the hour is odd
+                    continue
+            df = df.resample('15min')
+            df = df[df.T >= self.thresh]  # only keep wet days
+            df = df * self.per_hour  # Make it a rain rate
+            foo.append(df.quantile(np.arange(0,1.001,.001)))
+            labels.append(name)
+        quan = pd.DataFrame(foo).transpose()
+        quan.columns = labels
+        self.quantiles = quan
         
-        '''
-        fig = plt.figure(figsize=(16,4))
-
-        if interval is 'seasonal':
-            df_list = [df[1] for df in self.gage_mean.months]
-
-        if interval is 'diurnal':
-            df_list = [i[1] for i in self.gage_mean.hours if i[0]%2==0]
-            
-        if interval in self.months:
-            df = [df[1] for df in self.gage_mean.months][interval-self.months[0]]
-            df_list = [i[1] for i in df.groupby(df.index.hour) if i[0]%2==0]
-            fig.suptitle('{time_step} Rain Rate Distribution (excluding dry {time_step}) {interval}-{year}'.format(
-                         time_step=time_step, interval=interval, year=self.year), fontsize=20)
-            
-        if interval not in self.months:
-            fig.suptitle('{time_step} Rain Rate Distribution (excluding dry {time_step}) for {year}'.format(
-                         time_step=time_step, year=self.year), fontsize=20)
-        self.get_boxplots(df_list, fig, time_step, interval=interval)
-        self.get_distribution(time_step, interval)
-    
-    def plot_lots(self):
-        self.get_wet()
-        self.get_prob_wet('seasonal')
-        self.get_prob_wet('diurnal')
-        self.get_prob_wet('all')
-        self.get_quantiles(interval='seasonal')
-        self.get_quantiles(interval='seasonal', time_step='1H')
-        self.get_quantiles(interval='seasonal', time_step='1D')
-        self.get_quantiles(interval='diurnal')
-        self.get_quantiles(interval='diurnal', time_step='1H')
- 
+        fig = plt.figure(figsize=(len(wet_rates)*4/3,4))
+        ax = fig.add_subplot(111)
+        self.quantiles.plot(logy=True,ax=ax, legend=None)
+        ax.set_ylabel('Rain Rate (mm/hr)') 
+        title = '{ts} Rain Rate Distribution (excluding dry {ts})'
+        self.create_title(title, interval, gage, month, hour)
+        plt.title(self.title)
