4d16ee365342044d975cead6982f6a8a8a395f30
Julia Signell <jsignell@gmail.com>
2016-02-01 19:29:24 +0000

    these should make it easier to use the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rain',
+      version='0.1',
+      description='Tools for reading rain gage data files',
+      url='http://github.com/jsignell/rain-gage-tools',
+      author='Julia Signell',
+      author_email='jsignell@gmail.com',
+      license='MIT',
+      packages=['rain'],
+      zip_safe=False)
