4206326f368cd5b4d6c20c3ae0439327aaa6c983
Julia Signell <jsignell@gmail.com>
2016-02-01 22:02:20 +0000

    improved ease of install

--- rain/common.py
+++ rain/common.py
@@ -8,8 +8,10 @@ import posixpath
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from  mpld3 import plugins
-from JSAnimation import IPython_display
+try:
+    from JSAnimation import IPython_display
+except:
+    pass
 from matplotlib import animation
 plt.style.use('ggplot')
 
