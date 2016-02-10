99822e421974bc7071fe98aabf6c90c9eede4a05
Julia Signell <jsignell@gmail.com>
2016-02-10 17:41:24 +0000

    version 0.2 includes rain.read_nc() for initilizing Rain object using netcdf files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rain',
-      version='0.1',
+      version='0.2',
       description='Tools for reading rain gage data files',
       url='http://github.com/jsignell/rain-gage-tools',
       author='Julia Signell',
